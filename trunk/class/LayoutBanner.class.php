84d4374a51d0313c242af183062c3ad221dbe723
erinthonc <erinthonc@06635a0e-bd91-11dd-afb1-a7db9ebf18e3>
2008-11-29 21:05:32 +0000

    "atualizacao"

---
+++ trunk/class/LayoutBanner.class.php
@@ -0,0 +1,17 @@
+<?php
+	class LayoutBanner extends BannerHTML  {
+		public function EstruturaBanner($acao) {
+			switch ($acao) {
+				case "mostra":
+					$this->BannerMostra("Verificar Banners");
+				break;
+				case "add":
+					//$this->AdministracaoADD("Criar Usuários","return valida_usuario();","act/Administracao.act.php?acao=add","usuario","post");
+				break;
+				case "altera":
+					//$this->AdministracaoALT("Alterar Usuário","return valida_altusuario();","act/Administracao.act.php?acao=alt","altusuario","post");
+				break;
+			}
+		}
+	}
+?>
