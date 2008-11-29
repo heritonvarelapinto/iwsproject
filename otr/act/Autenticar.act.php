cdaddd11ba6d42e39e85c039d59dbba6dfc65c9c
erinthonc <erinthonc@06635a0e-bd91-11dd-afb1-a7db9ebf18e3>
2008-11-29 03:36:50 +0000

    "Autenticar.act.php atualizado"

--- otr/act/Autenticar.act.php
+++ otr/act/Autenticar.act.php
@@ -1,14 +1,30 @@
 <?php
-	
-	if(isset($_POST["acao"])) {
-		$usuario = $_POST["usuario"];
-			header("location: Administracao.act.php?acao=senha&user=$usuario");
-	}else{
-		function __autoload($classe)
-	    {
-	        require_once "../../class/".$classe.".class.php";
-	    }
+	function __autoload($classe)
+    {
+        require_once "../../class/".$classe.".class.php";
+    }
+
+	$acao =  "senha";	
+
+	if(isset($acao)) {
+		$administracao = new Administracao();
+		$administracaoDAO = new AdministracaoDAO();
 		
+		$clienteDAO = new ClienteDAO();
+		$cliente = $clienteDAO->getUsuarioPorNome("Oiter Busca");
+		
+		$usuario = "admin";//$_POST["usuario"];		
+		$administracao = $administracaoDAO->getUsuarioPorUsuario($usuario);
+		$senha = $administracao->geraSenha();
+		$setSenha = md5($senha);
+		
+		$administracao->setSenha($setSenha);		
+		
+		$administracaoDAO->UpdateSenha($administracao,$administracao->getIdadministracao());
+		
+		$administracao->EnviaDadosEmailAdministracao($cliente,$administracao,$senha);
+			header("location: ../index.php");
+	}else{				
 	    session_start();
 	    header("Cache-control: private");
 	    
