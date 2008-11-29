c9b138d8dc67105f9c35f841bd37d7bb1866fac6
erinthonc <erinthonc@06635a0e-bd91-11dd-afb1-a7db9ebf18e3>
2008-11-29 22:10:01 +0000

    "BannerHTML.class"

--- trunk/class/BannerHTML.class.php
+++ trunk/class/BannerHTML.class.php
@@ -2,11 +2,12 @@
 	class BannerHTML extends HTML {
 		function BannerMostra($titulo) { ?>	
 		<? 
-			if($_POST["idcategoria"]) {
-				$idcat = $_POST["idcategoria"];
-			}elseif($_GET["idcat"]) {
-				$idcat = $_GET["idcat"];
+			if($_POST["iddepartamento"]) {
+				$iddep = $_POST["iddepartamento"];
+			}elseif($_GET["iddep"]) {
+				$iddep = $_GET["iddep"];
 			}
+			echo $iddep;
 		?>				
 			<span class="TituloPage">• <?=$titulo;?></span>
 	        <br/>
@@ -16,45 +17,42 @@
 					<td  colspan="3" class="label2">Página </td>
 				</tr>
 				<tr class="Linha1Tabela">
-	                <form action="" name="categoria" method="post" onsubmit="return valida_dropmenu(document.categoria.idcategoria,'categoria');"/>
+	                <form action="" name="departamento" method="post" onsubmit="return valida_dropmenu(document.departamento.iddepartamento,'departamento');"/>
 	                <td height="20" align="right">Pagina:</td>
-	                <td >
-				        <select class="FORMbox" name="idcategoria">
-				            <option value="">--Selecione--</option>
-				            <option value="1000">Página Inicial</option>			            
-				            <?
-							$rs = mysql_query("select * from bairro ORDER BY bairro");
-							while($var = mysql_fetch_array($rs)) {
-								print('<option value="'.$var["idbairro"].'">'.$var["bairro"].'</option>');
-							}
-							?>			            
-				        </select>
+	                <td>
+		            <?
+		            	$departamento = new Departamento();
+		            	$departamentoDAO = new DepartamentoDAO();
+		            	$departamento = $departamentoDAO->Lista();
+		            	
+		            	$this->selectDepartamentosAdmin($departamento);
+					?>			            
 					</td>
 	                <td><input type="submit" style="width: 80px;" class="bttn2" value="Buscar" name="pesquisar"/></td>
 	                </form>
 	            </tr>			
 			</table>
-			<? if($idcat) { ?>		
+			<? if($iddep) { ?>		
 			<table width="558" cellspacing="1" cellpadding="4" border="0" class="BordaTabela">
 				<tr class="Linha1Tabela">
 					<td colspan="3">
-						<? //AdmBanners::mostraBannersTopo($idcat); ?>
+						<? $this->BannerMostraTopo($iddep); ?>
 					</td>
 				</tr>
 				<tr class="Linha1Tabela">
 					<td colspan="3">
-						<? //AdmBanners::mostraBannersPeq($idcat); ?>
+						<? //AdmBanners::mostraBannersPeq($iddep); ?>
 					</td>
 				</tr>
 				<tr class="Linha1Tabela">
 					<td colspan="3">
-						<? //AdmBanners::mostraBannersLat($idcat); ?>
+						<? //AdmBanners::mostraBannersLat($iddep); ?>
 					</td>
 				</tr>			
 			</table>
 			<? } ?>
 	<?	}
-	
+	/*
 		function data($id) {
 			$tempo = classAdmSQL::get("banners","idbanner",$id)->data;
 					
@@ -102,60 +100,77 @@
 		}
 	
 		function limpar($id) {
-			/*$query = "UPDATE banners SET banner = 'banner_home_46880.gif', url = 'http://', data = '0000-00-00 00:00:00' WHERE idbanner = '$id'";*/
+			//$query = "UPDATE banners SET banner = 'banner_home_46880.gif', url = 'http://', data = '0000-00-00 00:00:00' WHERE idbanner = '$id'";
 			$query = "DELETE FROM banners WHERE idbanner = '$id'";
 			$resultado = conexao::query($query);
 			if(!$resultado) { echo "Erro na função limpar:".mysql_error(); }
 			
 			return $resultado;
+		}*/
+		
+		/**
+		 * Pega a extensao do arquivo
+		 *
+		 * @param string $nome
+		 * @return string
+		 */
+		function pegaExt($nome) {
+			preg_match("/\.(gif|png|jpg|jpeg|swf){1}$/i", $nome, $ext);
+			return $ext[1];
 		}
 		
-		function mostraBannersTopo($idcat) { ?>		
+		function BannerMostraTopo($iddep) { ?>		
 			<table width="558" cellspacing="1" cellpadding="4" border="0" class="BordaTabela">
 				<tr class="TituloTabela">
-					<td  colspan="3" class="label2">Página [<? if($idcat == "1000") { echo "Página Inicial"; }else{ echo classAdmSQL::get("bairro","idbairro",$idcat)->bairro; } ?>]</td>
+					<td  colspan="3" class="label2">Página [<? if($iddep == "0") { echo "Página Inicial"; }else{ /*echo classAdmSQL::get("bairro","idbairro",$idcat)->bairro;*/ } ?>]</td>
 				</tr>			
 				<tr class="TituloTabela">
 					<td  colspan="3" class="label2">Banners Grande Topo (468x80)</td>
 				</tr>								
-				<? $rs = AdmBanners::getBannerRS("topo",$idcat);											
-						$i=1;
-						while ($banners = classAdmGeral::resultado($rs)) { ?>
-						<?
-							if($idcat == 1) {
-								$width = $banners->width; 
-							    $height = $banners->height;
-							    $largura = $width / 2;
-							    $altura = $height / 2;
-							}else{
-								$width = $banners->width; 
-							    $height = $banners->height;
-							    $largura = $width / 2;
-							    $altura = $height / 2;
-							}
-						?>
-							<tr class="Linha1Tabela">
-								<td width="25%" align="center">Banner <?=$i;?><br><input type="button" class="bttn4" value="Alterar" onclick="altBanner('<?=$banners->idbanner;?>','468','80');"><br><?AdmBanners::data($banners->idbanner);?></td>							
-								<td align="center">
-									<? if(AdmBanners::pegaExt($banners->banner) == "swf") { ?>													
-										<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="<?=$largura;?>" height="<?=$altura;?>" id="promocao" align="middle">
-										<param name="movie" value="../img/banners/<?=$banners->banner;?>" /><param name="quality" value="high" /><param name="wmode" value="transparent" /><param name="bgcolor" value="#ffffff" /><embed src="../img/banners/<?=$banners->banner;?>" quality="high" wmode="transparent" bgcolor="#ffffff" width="<?=$largura;?>" height="<?=$altura;?>" name="promocao" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
-										</object>
-									<? }else{ ?>
-										<img src="../act/geraBan.php?imagem=<?=$banners->banner;?>&w=<?=$largura;?>&h=<?=$altura;?>" border="0">
-									<? } ?>
-								</td>							
-							</tr>						
-						<? $i++; ?>		
-					<?	}				
+				<? 					
+					$banner = new Banner();
+					$bannerDAO = new BannerDAO();
+					$banner = $bannerDAO->ListaBannerPorDepartamentoPosicaoAdmin($iddep,"topo");
+
+					print_r($banner);
+					
+					$totBanner = count($banner);
+					
+					$i = $totBanner + 1;				
+									
+					if($idcat == 1) {
+						$width = $banner->getWidth(); 
+					    $height = $banner->getHeight();
+					    $largura = $width / 2;
+					    $altura = $height / 2;
+					}else{
+						$width = $banner->getWidth(); 
+					    $height = $banner->getHeight();
+					    $largura = $width / 2;
+					    $altura = $height / 2;
+					}
 				?>
-				<? if($idcat == 1000) { ?>
 				<tr class="Linha1Tabela">
-					<td colspan="2" align="center">Tamanho Máximo<br>480x80<br><br><input type="button" class="bttn2" value="Adicionar Banner" onclick="addBanner('<?=$idcat;?>','topo','<?=$i;?>','468','80')"></td>
+					<td width="25%" align="center">Banner <?=$i;?><br><input type="button" class="bttn4" value="Alterar" onclick="altBanner('<?=$banner->getIdbanner();?>','468','80');"><br><?='data';//AdmBanners::data($banners->idbanner);?></td>							
+					<td align="center">
+						<? if($this->pegaExt($banner->getBanner()) == "swf") { ?>													
+							<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="<?=$largura;?>" height="<?=$altura;?>" id="promocao" align="middle">
+							<param name="movie" value="../img/banners/<?=$banner->getBanner();?>" /><param name="quality" value="high" /><param name="wmode" value="transparent" /><param name="bgcolor" value="#ffffff" /><embed src="../images/banners/<?=$banner->getBanner();?>" quality="high" wmode="transparent" bgcolor="#ffffff" width="<?=$largura;?>" height="<?=$altura;?>" name="promocao" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+							</object>
+						<? }else{ ?>
+							<img src="../images/banners/<?=$banner->getBanner();?>" width="<?=$width;?>" height="<?=$height;?>" border="0">
+						<? } ?>
+					</td>							
+				</tr>						
+				<?					
+				?>
+				<? if($iddep == 0) { ?>
+				<tr class="Linha1Tabela">
+					<td colspan="2" align="center">Tamanho Máximo<br>480x80<br><br><input type="button" class="bttn2" value="Adicionar Banner" onclick="addBanner('<?=$iddep;?>','topo','<?=$i;?>','468','80')"></td>
 				</tr>
 				<? }else{ ?>
 					<tr class="Linha1Tabela">
-						<td colspan="2" align="center">Tamanho Máximo<br>480x80<br><br><input type="button" class="bttn2" value="Adicionar Banner" onclick="addBanner('<?=$idcat;?>','topo','<?=$i;?>','480','80')"></td>
+						<td colspan="2" align="center">Tamanho Máximo<br>480x80<br><br><input type="button" class="bttn2" value="Adicionar Banner" onclick="addBanner('<?=$iddep;?>','topo','<?=$i;?>','480','80')"></td>
 					</tr>
 				<? } ?>							
 			</table>
