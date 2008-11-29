c6a5c6de9db41dc5d656a61929c23c9303b1b005
erinthonc <erinthonc@06635a0e-bd91-11dd-afb1-a7db9ebf18e3>
2008-11-29 17:36:09 +0000

    "Arrumado o Login"

--- trunk/otr/act/Autenticar.act.php
+++ trunk/otr/act/Autenticar.act.php
@@ -28,6 +28,6 @@
 	    
 		$autentica = new Auth();
 		$autentica->autentica($_POST["usuario"],$_POST["senha"]);		
-		//$autentica->autentica("admin","thon");	
+		//$autentica->autentica("admin","tekrox");	
 	}	
 ?>
