1cac9c29104e4554ee7c1cfd7568bbf0bc2d595b
erinthonc <erinthonc@06635a0e-bd91-11dd-afb1-a7db9ebf18e3>
2008-12-03 03:46:20 +0000

    "tetse"

--- otr/teste.php
+++ otr/teste.php
@@ -4,7 +4,7 @@ function __autoload($classe)
         require_once "../class/".$classe.".class.php";
     }
 	
-    $acao = "altdep&iddepartamento=1";
+    $acao = "altdep&iddepartamento=4";
     
     $layoutDepartamento = new LayoutDepartamento();
 	$layoutDepartamento->EstruturaDepartamento($acao);
