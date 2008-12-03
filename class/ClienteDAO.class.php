b939a8ef4ad3ae50e0301e78ba45787cedcc5073
erinthonc <erinthonc@06635a0e-bd91-11dd-afb1-a7db9ebf18e3>
2008-12-03 15:47:03 +0000

    ""

--- class/ClienteDAO.class.php
+++ class/ClienteDAO.class.php
@@ -77,31 +77,5 @@ class ClienteDAO extends PDOConnectionFactory {
 		 
 		return $temp;
 	}
-	
-	
-	function checkErrors($err,$errno) {
-
-        //global $systemLog;
-        // Only thing that we need todo is define some variables
-        // And ask from RDBMS, if there was some sort of errors.
-
-        if($errno) {
-                // SQL Error occurred. This is FATAL error. Error message and 
-                // SQL command will be logged and aplication will teminate immediately.
-                $message = "The following SQL command ".$sql." caused Database error: ".$err.".";
-
-                //$message = addslashes("SQL-command: ".$sql." error-message: ".$message);
-                //$systemLog->writeSystemSqlError ("SQL Error occurred", $errno, $message);
-
-                print "Unrecowerable error has occurred. All data will be logged.";
-                print "Please contact System Administrator for help! \n";
-                print "<!-- ".$message." -->\n";
-                exit;
-
-        } else {
-                // Since there was no error, we can safely return to main program.
-                return;
-        }
-	}
 }
 ?>
