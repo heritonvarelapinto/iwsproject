13008086755d3f541763764045edc5b485cedab7
fernando.colnaghi <fernando.colnaghi@06635a0e-bd91-11dd-afb1-a7db9ebf18e3>
2008-12-28 23:25:48 +0000

    "Arquivo para leitura da pagina institucional "

---
+++ trunk/paginas.php
@@ -0,0 +1,154 @@
+<?php
+	function __autoload($classe) {
+		require_once "class/".$classe.".class.php";
+	}
+	
+	$id = $_GET['id'];
+	$titulo = $_GET['titulo'];
+	
+	$pagina = new Rodape();
+	$paginaDAO = new RodapeDAO();	
+	$pagina = $paginaDAO->getRodapePorId($id);
+	
+	$layout = new Layout();
+	$departamentos = new Departamento();
+	$departamentosDAO = new DepartamentoDAO();
+	$departamentos = $departamentosDAO->Lista();
+	
+	$banners = new Banner();
+	$bannerDAO = new BannerDAO();
+	$banners = $bannerDAO->ListaBannerPorDepartamentoPosicao("inicial","lateral",10);
+?>
+<html>
+<head>
+	<title>OiterBusca</title>
+	<meta http-equiv="Content-Type" content="text/html;iso-8859-1">
+	<?=$layout->getTheme("");?>
+	<link rel="shortcut icon" href="icones/favicon.ico" >
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script>
+		varover = 0;
+		$(document).ready(function(){
+				var nav = $('#userAgent').html(navigator.userAgent);
+				
+				$("#departamentos").hide();
+				
+				if(varover == 0) {
+					$("a.showAll").mouseover ( function(event){
+						//Seta a posicao do departamentos pro tamanho do browser
+						var offset = $("a.showAll").offset();
+						offset.top = $("a.showAll").offset().top + 18;
+						
+						$("#departamentos").css(offset);
+						$("#departamentos").fadeIn(300);
+						varover = 1;
+					} );
+				}
+
+				$("a.motors").mouseover ( function(event){
+					$("#departamentos").fadeOut(100);
+					varover = 0;
+				} );
+				
+				$("#departamentos").mouseout ( function(event){
+					$("#departamentos").hide();
+					varover = 0;
+				} );
+
+				$("#departamentos").mouseover ( function(event){
+					$("#departamentos").show();
+				} );
+				
+				$(".menuPesquisa").mouseover ( function(event){
+					$("#departamentos").fadeOut(100);
+					varover = 0;
+				} );
+				
+		 });
+	</script>
+</head>
+
+<body>
+<div>
+	<div id="main">
+		<div id="barraLogo">
+			
+			<table class="logo">
+				<tr>
+					<td align="left"><img src="images/logos/logo.jpg" alt="OiterBusca um site "/></td>
+					<td align="right" style="padding-right: 10px;"><?=$layout->bannersTopo($bannerDAO->ListaBannerPorDepartamentoPosicao("inicial","topo",1));?></td>
+					<td align="right" width="186"><?=$layout->bannersTopo($bannerDAO->ListaBannerPorDepartamentoPosicao("inicial","topopeq",1));?></td>
+				</tr>
+			</table>
+			
+		</div>
+		<div id="content">
+			<!-- Inicio Header -->
+			<div id="header">
+				<!-- Barra Pesquisa -->
+				<div class="menuPesquisa">
+					<div class="menuPesquisaEsq">
+						<div class="menuPesquisaDir">
+							<form method="POST" action="pesquisa.php">
+							<table border="0" cellpadding="2" cellspacing="2" class="tablePesquisa">
+								<tr>
+									<td><?=$layout->input("pesquisa","inputPesquisa");?></td>
+									<td><?=$layout->selectDepartamentos($departamentos);?></td>
+									<td><?=$layout->button("btEnviar","button","Buscar");?> </td>
+									<!--<td><?=$layout->button("btEnviar","button","Pesquisa avançada");?> </td>-->
+								</tr>
+							</table>
+							</form>
+						</div>
+					</div>
+				</div>
+				<!-- Fim Barra Pesquisa -->
+		
+				<!-- Barra Itens Menu -->
+				<div class="menuItens">
+					<div class="menuItensEsq">
+						<div class="menuItensDir">
+							<ul>
+								<li>
+									<a href="#" class="showAll">Departamentos <img border="0" src="images/seta.gif"></a>
+									<div id="departamentos" style="display: none;">
+										<ul>
+											<?=$layout->menuSuperiorDepartamentos($departamentos);?>
+										</ul>
+									</div>
+								</li>
+								<li class="novo"><a href="#" class="motors">Motors</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<!-- Fim Barra Itens Menu -->
+			</div>
+			<!-- Fim do Header -->
+			<div id="corpo">
+				<div class="menu">
+						<?=$layout->menuDepartamentos($departamentos);?>
+						<?=$layout->bannersEsquerda($bannerDAO->ListaBannerPorDepartamentoPosicao("inicial","lateralesq",3));?>
+				</div>
+				<div class="miolo">
+					<h3><?=$pagina->titulo;?></h3>
+					<p>
+						<?=nl2br($pagina->texto);?>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div id="lateralDireita">
+			<?=$layout->bannersLaterais($banners);?>
+		</div>
+		<div id="lateralDireita">
+			<?=$layout->enquete();?>
+		</div>
+		<div id="lateralDireita">
+			<?=$layout->boletim();?>
+		</div>
+		<?=$layout->rodape();?>
+	</div>
+</div>
+</body>
+</html>
