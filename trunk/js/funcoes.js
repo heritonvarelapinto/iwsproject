fa9eaa41d5925c98aa26912740b7f4e2578051d7
fernando.colnaghi <fernando.colnaghi@06635a0e-bd91-11dd-afb1-a7db9ebf18e3>
2009-01-05 22:24:39 +0000

    ""

--- trunk/js/funcoes.js
+++ trunk/js/funcoes.js
@@ -1,24 +1,8 @@
-function detalhe(pagina, titulo, width, height){
-	topW  = (screen.availHeight / 2) - (width / 2);
-	leftW = (screen.availWidth / 2) - (height / 2);
-
-	try{
-		window.open('/anunciante.php?id='+id, '_blank','top=' + topW + ', left=' + leftW + ', height=' + height + ', width=' + width + ', status=no, menubar=no, resizable=no, scrollbars=no, toolbar=no, location=no, directories=no');
-	} catch(e){	}
-	
-	if(!myWindow) {
-		alert("Verifique seu anti-popup.")
-	}
-}
-
 function abrirDestaque(id, titulo, largura, altura) {
 	topW  = (screen.availHeight / 2) - (altura / 2);
 	leftW = (screen.availWidth / 2) - (largura / 2);
 
-	opcoes = 'top=' +topW+ ', left=' +leftW+ ', height=' +altura+ ', width=' +largura;
-	
-	alert(opcoes)
-	
+	opcoes = 'top=' +topW+ ', left=' +leftW+ ', height=' +altura+ ', width=' +largura;	
 	
 	myWindow = window.open(id, '', opcoes + ', status=no, menubar=no, resizable=no, scrollbars=no, toolbar=no, location=no, directories=no');
 	
@@ -26,3 +10,22 @@ function abrirDestaque(id, titulo, largura, altura) {
 		alert("Verifique seu anti-popup.")
 	}
 }
+	
+	function localizacao() {
+		$("#map").show();
+		$("#contato").hide();
+		$("#foto").hide();
+	}
+
+	function contatoAnuncio() {
+		$("#map").hide();
+		$("#contato").show();
+		$("#foto").hide();
+	}
+	
+	function verFoto(imagem) {
+		$("#map").hide();
+		$("#contato").hide();
+		$("#foto").html("<img src = " + imagem + ">");
+		$("#foto").show();
+	}
