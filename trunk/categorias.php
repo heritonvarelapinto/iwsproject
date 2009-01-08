d09399db7d59f8ec9dc4d7ee878a1d5f5c940c86
fernando.colnaghi <fernando.colnaghi@06635a0e-bd91-11dd-afb1-a7db9ebf18e3>
2009-01-08 19:52:28 +0000

    ""

--- trunk/categorias.php
+++ trunk/categorias.php
@@ -62,25 +62,7 @@
 			<!-- Inicio Header -->
 			<div id="header">
 				<? $layout->barraPesquisa($departamentos); ?>
-				<!-- Barra Itens Menu -->
-				<div class="menuItens">
-					<div class="menuItensEsq">
-						<div class="menuItensDir">
-							<ul>
-								<li>
-									<a href="#" class="showAll">Departamentos <img border="0" src="<?=$layout->image_path;?>images/seta.gif"></a>
-									<div id="departamentos" style="display: none;">
-										<ul>
-											<?=$layout->menuSuperiorDepartamentos($departamentos);?>
-										</ul>
-									</div>
-								</li>
-								<!--<li class="novo"><a href="#" class="motors">Motors</a></li>-->
-							</ul>
-						</div>
-					</div>
-				</div>
-				<!-- Fim Barra Itens Menu -->
+				<? $layout->barraItensMenu($departamentos);?>
 			</div>
 			<!-- Fim do Header -->
 			<div id="corpo">
@@ -101,8 +83,6 @@
 				</div>
 				<div class="miolo">
 					<? 
-						$layout->breadcrumb($id,$sub);
-					
 						if($_GET['totalPP']) {
 							$totalPorPagina = $_GET['totalPP'];
 						} else {
@@ -120,74 +100,51 @@
 						}
 						
 						$totAnuncios = count($anuncio);
-						$paginas = ceil($totAnuncios / $totalPorPagina);				
-											
-						if($_GET['pag']) {
-							$pagina = $_GET['pag'];
-						} else {
-							$pagina = 0;
-						}
-						
-						echo "<div id=\"resultados\"><ul>
-							<li>Encontrados ".$totAnuncios." anúncios</li>
-							<li class=\"totalpagina\">Resultados por página
-							<select name=\"totalPP\" onchange=\"reload('".$id."','".$sub."','".$pagina."',this.value,'".$layout->image_path."')\">";
-							?>
-									<option value="10" <? if($totalPorPagina == 10) { echo "selected"; } ?> >10</option>
-									<option value="15" <? if($totalPorPagina == 15) { echo "selected"; } ?> >15</option>
-									<option value="30" <? if($totalPorPagina == 30) { echo "selected"; } ?> >30</option>
-									<option value="50" <? if($totalPorPagina == 50) { echo "selected"; } ?> >50</option>
-							<?
-							echo "</select>
-							</li>
-						</ul></div>";
-						
-						$inicio = $pagina * $totalPorPagina;
-					
-						$regra .= "Order by acessos DESC, nome ASC";
-						
-						$anuncio = $anuncioDAO->Paginacao($regra, $inicio, $totalPorPagina);
-						$totAnuncios = count($anuncio);
-											
-						for($i = 0 ; $i < $totAnuncios;$i++) {
-							if($i % 2) {
-								$cor = "#DDDDDD";
+						if($totAnuncios > 0) {
+							$paginas = ceil($totAnuncios / $totalPorPagina);				
+												
+							if($_GET['pag']) {
+								$pagina = $_GET['pag'];
 							} else {
-								$cor = "#EEEEEE";
+								$pagina = 0;
 							}
-							echo "<div id=\"anuncios\" style=\"border-bottom: 1px dashed ".$cor."\">";
-							echo "<h3 onclick=\"contaAcesso('".$anuncio[$i]->getIdanuncio()."');abrirDestaque('".$layout->image_path."anunciante.php?id=".$anuncio[$i]->getIdAnuncio()."&p=info','".$anuncio[$i]->getNome()."',700,500)\">".$anuncio[$i]->getNome()."</h3>";
-							echo "<p class=\"direita\">";
-							echo $anuncio[$i]->getEndereco().", ".$anuncio[$i]->getNumero()." ".$anuncio[$i]->getComplemento();
-							echo "<br>";
-							echo $anuncio[$i]->getBairro()." - ".$anuncio[$i]->getCidade()." - ".$anuncio[$i]->getEstado();
-							echo "<br>";
-							if($anuncio[$i]->getEmail() != "") echo "<b>E-mail: </b>".$anuncio[$i]->getEmail();
-							echo "<br>";
-							if($anuncio[$i]->getSite() != "") echo "<b>Site: </b><a class=\"site\" onclick=\"contaAcesso('".$anuncio[$i]->getIdanuncio()."');abrirSite('".$anuncio[$i]->getSite()."')\">".$anuncio[$i]->getSite()."</a>";
-							echo "<br>";
-							echo "<br>";
-							echo "<a onclick=\"contaAcesso('".$anuncio[$i]->getIdanuncio()."');this.innerHTML = '".$anuncio[$i]->getTelefones()."'\" id=\"telefone\">Clique aqui para ver o telefone</a>";
-							echo "<br>";
-							//echo "<a onclick=\"abrirDestaque('".$layout->image_path."anunciante.php?id=".$anuncio[$i]->getIdAnuncio()."&p=info','".$anuncio[$i]->getNome()."',700,500)\" id=\"mais_info\" title=\"Mais informações\">Saiba um pouco mais sobré nós</a>";
-							//echo "<img src=\"\".$layout->image_path."images/info.png\" alt=\"Mais informações\" onclick=\"abrirDestaque('".$layout->image_path."anunciante.php?id=".$anuncio[$i]->getIdAnuncio()."&p=info','".$anuncio[$i]->getNome()."',700,500)\">";
-							echo "</p>";
-							echo "<p onclick=\"contaAcesso('".$anuncio[$i]->getIdanuncio()."');abrirDestaque('".$layout->image_path."anunciante.php?id=".$anuncio[$i]->getIdAnuncio()."&p=info','".$anuncio[$i]->getNome()."',700,500)\" class=\"esquerda\">";
-							echo "<img src=\"".$layout->image_path."images/logos/".$anuncio[$i]->getLogo()."\" class=\"borda\">";
-							echo "</p>";
-							echo "</div>";
-						}
+							
+							
+							$layout->breadcrumb($id,$sub);
+							
+							echo "<div id=\"resultados\"><ul>
+								<li>Encontrados ".$totAnuncios." anúncios</li>
+								<li class=\"totalpagina\">Resultados por página
+								<select name=\"totalPP\" onchange=\"reload('".$id."','".$sub."','".$pagina."',this.value,'".$layout->image_path."')\">";
+								?>
+										<option value="10" <? if($totalPorPagina == 10) { echo "selected"; } ?> >10</option>
+										<option value="15" <? if($totalPorPagina == 15) { echo "selected"; } ?> >15</option>
+										<option value="30" <? if($totalPorPagina == 30) { echo "selected"; } ?> >30</option>
+										<option value="50" <? if($totalPorPagina == 50) { echo "selected"; } ?> >50</option>
+								<?
+								echo "</select>
+								</li>
+							</ul></div>";
+							
+							$inicio = $pagina * $totalPorPagina;
 						
-						$pagina = $pagina + 1;
-						$layout->paginacaoAnuncio($pagina,$paginas,$id,$sub,$totalPorPagina);
+							$regra .= "Order by detalhe DESC, acessos DESC, nome ASC";
+							
+							$anuncio = $anuncioDAO->Paginacao($regra, $inicio, $totalPorPagina);
+							
+							$layout->mostraAnuncios($anuncio);
+							
+							$pagina = $pagina + 1;
+							$layout->paginacaoAnuncio($pagina,$paginas,$id,$sub,$totalPorPagina);
+						} else {
+							echo "Sem anúncios no cadastro";
+						}
 					?>
 					
 				</div>
 			</div>
 		</div>
-		<div id="lateralDireita">
-			<?=$layout->bannersLaterais($banners);?>
-		</div>
+		<?=$layout->lateralDireita($banners);?>
 		<?=$layout->rodape();?>
 	</div>
 </div>
--- trunk/paginas.php
+++ trunk/paginas.php
@@ -20,54 +20,7 @@
 	$banners = $bannerDAO->ListaBannerPorDepartamentoPosicao("inicial","lateral",10);
 ?>
 <html>
-<head>
-	<title>OiterBusca - <?=$pagina->titulo;?></title>
-	<meta http-equiv="Content-Type" content="text/html;iso-8859-1">
-	<?=$layout->getTheme("");?>
-	<link rel="shortcut icon" href="<?=$layout->image_path;?>icones/favicon.ico" >
-	<script type="text/javascript" src="<?=$layout->image_path;?>js/jquery.js"></script>
-	<script>
-		varover = 0;
-		$(document).ready(function(){
-				var nav = $('#userAgent').html(navigator.userAgent);
-				
-				$("#departamentos").hide();
-				
-				if(varover == 0) {
-					$("a.showAll").mouseover ( function(event){
-						//Seta a posicao do departamentos pro tamanho do browser
-						var offset = $("a.showAll").offset();
-						offset.top = $("a.showAll").offset().top + 18;
-						
-						$("#departamentos").css(offset);
-						$("#departamentos").fadeIn(300);
-						varover = 1;
-					} );
-				}
-
-				$("a.motors").mouseover ( function(event){
-					$("#departamentos").fadeOut(100);
-					varover = 0;
-				} );
-				
-				$("#departamentos").mouseout ( function(event){
-					$("#departamentos").hide();
-					varover = 0;
-				} );
-
-				$("#departamentos").mouseover ( function(event){
-					$("#departamentos").show();
-				} );
-				
-				$(".menuPesquisa").mouseover ( function(event){
-					$("#departamentos").fadeOut(100);
-					varover = 0;
-				} );
-				
-		 });
-	</script>
-</head>
-
+<?=$layout->head();?>
 <body>
 <div>
 	<div id="main">
@@ -85,44 +38,8 @@
 		<div id="content">
 			<!-- Inicio Header -->
 			<div id="header">
-				<!-- Barra Pesquisa -->
-				<div class="menuPesquisa">
-					<div class="menuPesquisaEsq">
-						<div class="menuPesquisaDir">
-							<form method="POST" action="pesquisa.php">
-							<table border="0" cellpadding="2" cellspacing="2" class="tablePesquisa">
-								<tr>
-									<td><?=$layout->input("pesquisa","inputPesquisa");?></td>
-									<td><?=$layout->selectDepartamentos($departamentos);?></td>
-									<td><?=$layout->button("btEnviar","button","Buscar");?> </td>
-									<!--<td><?=$layout->button("btEnviar","button","Pesquisa avançada");?> </td>-->
-								</tr>
-							</table>
-							</form>
-						</div>
-					</div>
-				</div>
-				<!-- Fim Barra Pesquisa -->
-		
-				<!-- Barra Itens Menu -->
-				<div class="menuItens">
-					<div class="menuItensEsq">
-						<div class="menuItensDir">
-							<ul>
-								<li>
-									<a href="#" class="showAll">Departamentos <img border="0" src="<?=$layout->image_path;?>images/seta.gif"></a>
-									<div id="departamentos" style="display: none;">
-										<ul>
-											<?=$layout->menuSuperiorDepartamentos($departamentos);?>
-										</ul>
-									</div>
-								</li>
-								<li class="novo"><a href="#" class="motors">Motors</a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<!-- Fim Barra Itens Menu -->
+				<? $layout->barraPesquisa($departamentos);?>
+				<? $layout->barraItensMenu($departamentos);?>
 			</div>
 			<!-- Fim do Header -->
 			<div id="corpo">
@@ -143,15 +60,7 @@
 				</div>
 			</div>
 		</div>
-		<div id="lateralDireita">
-			<?=$layout->bannersLaterais($banners);?>
-		</div>
-		<div id="lateralDireita">
-			<?=$layout->enquete();?>
-		</div>
-		<div id="lateralDireita">
-			<?=$layout->boletim();?>
-		</div>
+		<?=$layout->lateralDireita($banners);?>
 		<?=$layout->rodape();?>
 	</div>
 </div>
