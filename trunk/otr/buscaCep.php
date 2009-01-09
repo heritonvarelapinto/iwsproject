5c4948b6b75297d50bc81ca98b53ea53ad56bce8
fernando.colnaghi <fernando.colnaghi@06635a0e-bd91-11dd-afb1-a7db9ebf18e3>
2009-01-09 21:42:49 +0000

    ""

--- trunk/otr/buscaCep.php
+++ trunk/otr/buscaCep.php
@@ -6,11 +6,8 @@
 	$cepget = $_POST['cep'];
 	
 	$cep = $anuncioHTML->busca_cep($cepget);
-	
-	foreach ($cep as $dados => $valor) {
-		$resultado .= utf8_encode($valor).";";
-	}
 		
-	echo $resultado;
+	echo $cep;
+	
 ?>
 	
