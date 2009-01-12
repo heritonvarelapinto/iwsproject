663c12f85f31a808d5722f1204f72eab53d15de5
fernando.colnaghi <fernando.colnaghi@06635a0e-bd91-11dd-afb1-a7db9ebf18e3>
2009-01-12 01:42:42 +0000

    ""

--- act/correio.php
+++ act/correio.php
@@ -20,12 +20,14 @@
 	if($_POST['unid'] == $_SESSION['sid_textoCaptcha']) {
 		$headers = "Content-type: text/html; charset=iso-8859-1\n";
 		$headers .= "From: ".$_POST['nome']." <".$_POST['email'].">";
-		$assunto = "OITERBUSCA - ".$_POST['assunto'];
+		$assunto = $_POST['assunto']. " - Contato pelo OITERBUSCA";
+		if($_POST['telefone']) $telefone = '<p>Telefone: '.$_POST['telefone'].'</p>';
 		$mensagem = '<html dir="ltr">
 					    <head>
 					    </head>
 					    <body spellcheck="false">
-					        <p>'.$_POST['mensagem'].'</p>
+					    	'.$telefone.'
+					        <p>'.nl2br($_POST['mensagem']).'</p>
 					    </body>
 					</html>';			
 		if(mail($anuncio->getEmail(),$assunto,$mensagem,$headers)) {
