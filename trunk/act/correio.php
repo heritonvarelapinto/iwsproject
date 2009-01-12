446ff50d06476aed97aa6d21e46a8f72d334280b
fernando.colnaghi <fernando.colnaghi@06635a0e-bd91-11dd-afb1-a7db9ebf18e3>
2009-01-12 01:29:55 +0000

    ""

--- trunk/act/correio.php
+++ trunk/act/correio.php
@@ -1,8 +1,36 @@
 <?
 	session_start();
 	
+	function __autoload($classe) {
+		require_once "../class/".$classe.".class.php";
+	}
+	
+	/*$cliente = new Cliente();
+	$clienteDAO = new ClienteDAO();
+	
+	$cliente = $clienteDAO->getUsuarioPorNome("Oiter Busca");
+	
+	print_r($cliente);*/
+	
+	$anuncio = new Anuncio();
+	$anuncioDAO = new AnuncioDAO();
+	
+	$anuncio = $anuncioDAO->getAnuncioPorId($_POST['id']);
+	
 	if($_POST['unid'] == $_SESSION['sid_textoCaptcha']) {
-		echo true;
+		$headers = "Content-type: text/html; charset=iso-8859-1\n";
+		$headers .= "From: ".$_POST['nome']." <".$_POST['email'].">";
+		$assunto = "OITERBUSCA - ".$_POST['assunto'];
+		$mensagem = '<html dir="ltr">
+					    <head>
+					    </head>
+					    <body spellcheck="false">
+					        <p>'.$_POST['mensagem'].'</p>
+					    </body>
+					</html>';			
+		if(mail($anuncio->getEmail(),$assunto,$mensagem,$headers)) {
+			echo true;
+		}
 	} else {
 		echo false;
 	}
